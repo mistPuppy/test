e0de936931c5a369e84c18e0c3bcefae0bad9c34
chiuboru <microsoftdeanchiu@gmail.com>
2022-04-11 14:13:35 +0800

    0411邱柏儒跟奕慈上傳很久喔

---
+++ 0411/0411 ubike_card_num.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>新增一卡通</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+
+
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    
+    <main id="main">
+        <!-- ======= 整個版面 ======= -->
+        <section id="blog" class="blog" style="margin-left: 15%;">
+            <div class=" container" data-aos="fade-up">
+            <div class="row">
+                
+                <!-- 右側新增卡片區 -->
+                <div class="col-lg-10 entries">
+                    <article class="entry">
+                        <center>
+                            <h1 class="entry-title" style="margin-bottom:30px; font-size: 50px; ">
+                                <a href="#">新增一卡通</a>
+                            </h1>
+                        </center>
+                        <!-- ======= 一卡通 ======= -->
+                        <div class="container">
+                            <!-- 輸入訊息 -->
+                            <section id="contact" class="contact">
+                                <div class="container">
+                                    <div class="row">
+                                        <div>
+
+                                            <form action="forms/contact.php" method="post" role="form"
+                                                class="php-email-form">
+                                                <h4>OOO，您好:</h4>
+                                                <br>
+                                                <center>
+                                                    <div class="input-group mb-3" style="width:80%;">
+                                                        <span class="input-group-text" id="basic-addon1">帳號</span>
+                                                        <input type="text" class="form-control" placeholder="**********"
+                                                            aria-label="Username" aria-describedby="basic-addon1"
+                                                            readonly>
+                                                    </div>
+                                                </center>
+                                                <center>
+                                                <div class="input-group mb-3" style="width:80%;">
+                                                    <span class="input-group-text" >卡號:</span>
+                                                    <input type="text" class="form-control" placeholder="新增卡號"
+                                                        aria-label="Amount (to the nearest dollar)">
+
+                                                </div>
+                                                </center>
+                                                <br>
+                                                <div class="text-center"><button type="submit"><a href="ubike_card_manage.html" style="color: aliceblue;">送出</a></button></div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            </div>
+        </section>
+    </main>
+
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
